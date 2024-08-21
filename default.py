40cb943eb54a316bb7cf472ccb1bf1e31afbad63
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2024-08-21 14:11:30 -0500

    Update default.py

--- default.py
+++ default.py
@@ -1,1 +1,7 @@
-
+input = input("Do you like Nyan Cat? ").strip().lower()
+if input in ["yes", "y", "yeah", "yep"]:
+    print("Nyan Cat is awesome!")
+elif input in ["no", "n", "nope"]:
+    print("That's too bad.")
+else:
+    print("I don't understand that response.")
